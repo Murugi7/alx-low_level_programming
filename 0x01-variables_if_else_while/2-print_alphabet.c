0dbd04bde6ccb5a7e155b9a53c200189e04e5a93
Murugi7 <murugimwangi390@gmail.com>
2022-06-08 10:44:10 -0700

    The alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * description: use lowercase followed by newline
+ * return: (0)
+*/
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch = ++)
+{
+putchar(char);
+}
+putchar(10);
+return (0);
+}
